0c2d6f039aac5d624e014afb3540cce9eef99747
rob1bouchet <rob1bouchet@gmail.com>
2021-12-02 18:53:52 +0100

    exercice-6 shell script

---
+++ exercice-6/my_ansible.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+mkdir -p documents/{gamez,images,work/{code,plannings}}
+touch meeting_notes.txt documents/gamez/csgo.exe documents/work/plannings/{january,february,march,april}.xlsx
+exit 0
