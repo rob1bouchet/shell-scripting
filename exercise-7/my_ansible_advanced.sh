153dd791f63ea8a72e0931eddd82fa652c331f44
rob1bouchet <rob1bouchet@gmail.com>
2021-12-08 13:56:50 +0100

    added exercice-7 new error and README.txt

---
+++ exercise-7/my_ansible_advanced.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+if [ ! -z "$2" ]
+then
+echo "Usage: ./my ansible advanced.sh [username]"
+exit 1
+
+elif [ ! -z "$1" ] && [ ! -d "documents" ]
+then
+echo "directory documents must exist"
+exit 2
+
+elif [ ! -z "$1" ] && [ -d "documents" ]
+then
+mkdir -p documents/$1/{gamez,images,work/{code,plannings}}
+touch -a documents/$1/{gamez/csgo.exe,work/plannings/{january,february,march,april}.xlsx}
+
+elif [ -z "$1" ]
+then
+mkdir -p documents/{gamez,images,work/{code,plannings}}
+touch -a meeting_notes.txt documents/{gamez/csgo.exe,work/plannings/{january,february,march,april}.xlsx}
+
+fi
