5fd844edaf02273f23cb18b5b1a1e45cfdc6df82
ThibaultGRISVAL <94956636+ThibaultGRISVAL@users.noreply.github.com>
2021-12-14 09:44:17 +0100

    Exercise 5 - Program Code

---
+++ exercise-5/salutation_advanced.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if (($# < 2)) || (($# > 2)); then
+    	echo "Usage : ./salutation.sh firstname lastname"
+    	exit 1
+
+elif [ $1 = $2 ]; then
+	echo "Firstname and lastname cannot be the same !"
+	exit 2
+
+fi
+	echo "Hello $1 $2, have a nice day !"
